355ad51f450ebe7aac607f9e4788c7281362ec7f
Willie van Greunen <willietodo@gmail.com>
2026-05-19 16:53:40 +0200

    Expand product list in API response
    Added more sample products to the API response.

--- index.cjs
+++ index.cjs
@@ -3,7 +3,11 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello!'));
 app.get('/api/products', (req, res) => {
-  res.json([{ id: 1, name: 'Sample Product' }]);
+  res.json([
+    { id: 1, name: 'Sample Product' },
+    { id: 2, name: 'Another Product' },
+    { id: 3, name: 'Third Product' }
+  ]);
 });
 
 const PORT = process.env.PORT || 10000;
